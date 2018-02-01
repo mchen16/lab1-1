8d7dba153f3095f9159eb26cbc77f84e9085bf94
mchen16 <michael_chen@college.harvard.edu>
2018-02-01 15:32:35 -0500

    exercise7

--- lab1.ml
+++ lab1.ml
@@ -111,6 +111,16 @@ does *not* correspond to the abstract syntax above.
 Exercise 4: Draw the tree that the concrete syntax "- 5 - 3" does
 correspond to. Check it with a member of the course staff if you'd
 like.
+
+      -
+      ^
+     / \
+    /   \
+   -     3
+   |
+   |
+   5
+
 ......................................................................*)
 
 
@@ -124,24 +134,22 @@ expressions below? Test your solution by uncommenting the examples
 error is generated.
 ......................................................................*)
 
-(*   <--- remove this start of comment line
 
-let exercise5a : ??? = 42 ;;
+let exercise5a : int = 42 ;;
 
-let exercise5b : ??? =
+let exercise5b : string =
   let greet y = "Hello " ^ y
   in greet "World!";;
 
-let exercise5c : ???  =
+let exercise5c : int * float -> int  =
   fun (x, y) -> x + int_of_float y ;;
 
-let exercise5d : ??? =
+let exercise5d : int -> bool =
   fun x -> x < x + 1 ;;
 
-let exercise5e : ??? =
+let exercise5e : bool -> bool list =
   fun x -> if x then [x] else [] ;;
 
-remove this end of comment line too ----> *)
 
 (*======================================================================
 Part 3: First-order functional programming
@@ -170,10 +178,17 @@ to the list containing the elements 3, 4, and 5? You'll want to
 replace the "[]" with the correct functional call.
 ......................................................................*)
 
-let square_all (lst : int list) : int list =
-  failwith "square_all not implemented" ;;
+let rec square_all (lst : int list) : int list =
+  match lst with
+  | [] -> []
+  | hd :: tl -> (hd * hd) :: square_all tl ;;
 
-let exercise6 = [] ;;
+let exercise6 = [3;4;5] ;;
+
+let square_1 (a : int) : int = a * a;;
+let square_all_1 (lst : int list) : int list = List.map square_1 lst;;
+
+let square_all_2 lst = List.map (fun x -> x * x) lst;;
 
 (*......................................................................
 Exercise 7: Define a recursive function that sums an integer
